2ac78a2aa962c8c6b22466347c306e0f8e814321
Arunbalaji Srinivasan <59905487+Arunbalaji-Srinivasan@users.noreply.github.com>
2020-05-30 18:13:24 -0400

    Adding new files
    Adding new files

---
+++ README.md
@@ -0,0 +1,17 @@
+# React Native - Mobile Flashcards
+
+- This application is built to test the understanding of react native
+- Users can create decks and add questions to each deck
+- User can take a quiz after adding questions
+- The result will be displayed after completing the quiz
+
+# Installation Steps:
+
+    1. Clone this git repository
+    2. Navigate to mobile-flashcards
+    3. npm install
+    4. npm start
+    5. Install expo from play store
+    6. Open project using 'expo' app by scanning QR code
+
+
---
+++ components/Deck.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet,Button } from "react-native";
+import {connect} from "react-redux"
+import { purple,white,lightPurp } from "../utils/color";
+class Deck extends Component {
+  render() {
+      const {deck} =  this.props
+     return (
+     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+     <View key={deck.title} style={styles.deckContainer}>
+       <View>
+         <Text style={styles.deckText}>{deck.title}</Text>
+       </View>
+       <View>
+         <Text style={styles.cardText}>
+           {deck.questions.length} cards
+         </Text>
+       </View>
+     </View>
+     <Button
+          title="Start Quiz"
+          onPress={() => this.props.navigation.navigate('Quiz', {title: deck.title})}
+        />
+        <Text>{`\n`}</Text>
+      <Button title="Add Question" buttonStyle={styles.button} onPress={() => this.props.navigation.navigate('AddQuestion', {title: deck.title})}/>
+     </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+    button:{
+        marginBottom: 16,
+        color: purple,
+    },
+  
+    title: {
+      fontSize: 40,
+      textAlign: "center",
+      marginBottom: 16,
+      color: purple,
+    },
+    deckContainer: {
+      alignItems: "center",
+      justifyContent: "center",
+      flexBasis: 120,
+      minHeight: 120,
+      borderWidth: 1,
+      borderColor: "#aaa",
+      backgroundColor: white,
+      borderRadius: 5,
+      width:400,
+      marginBottom: 10,
+    },
+    deckText: {
+      fontSize: 28,
+    },
+    cardText: {
+      fontSize: 18,
+      color: lightPurp,
+    },
+  })
+
+function mapStateToProps(decks,{navigation,route}){
+    const title = route.params.title;
+    const deck = decks[title];
+    return {
+        deck
+    }
+}
+
+export default connect(mapStateToProps)(Deck)
